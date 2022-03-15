56fdd9b2f41cd0e2153f285c3e7c49dea981242f
Arthur BLANC <arthur.blanc@outlook.com>
2022-03-15 16:14:07 +0100

    A-Propos mobile page + add Accordion components

---
+++ src/components/Accordion/index.jsx
@@ -0,0 +1,58 @@
+import React, { useState, useRef } from 'react'
+import chevron from '../../assets/chevron.svg'
+
+import './style.css'
+
+function Accordion(props) {
+	const [setActive, setActiveState] = useState('')
+	const [setHeight, setHeightState] = useState('0px')
+	const [setRotate, setRotateState] = useState('accordion__icon')
+
+	const content = useRef(null)
+
+	function toggleAccordion() {
+		setActiveState(setActive === '' ? 'active' : '')
+		setHeightState(
+			setActive === 'active' ? '0px' : `${content.current.scrollHeight}px`
+		)
+		setRotateState(
+			setActive === 'active'
+				? 'accordion__icon'
+				: 'accordion__icon rotate'
+		)
+	}
+
+	const contentArray = []
+	if (!Array.isArray(props.content)) {
+		contentArray.push(props.content)
+	} else {
+		for (let i = 0; i < 9; i++) {
+			contentArray.push(props.content[i])
+		}
+	}
+
+	return (
+		<div className="accordion__section">
+			<button
+				className={`accordion ${setActive}`}
+				onClick={toggleAccordion}
+			>
+				<p className="accordion__title">{props.title}</p>
+				<img src={chevron} className={`${setRotate}`} />
+			</button>
+			<div
+				ref={content}
+				style={{ maxHeight: `${setHeight}` }}
+				className="accordion__content"
+			>
+				<div className="accordion__text">
+					{contentArray.map((content, index) => (
+						<div key={`${content}-${index}`}>{content}</div>
+					))}
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default Accordion
--- src/pages/A-Propos/index.jsx
+++ src/pages/A-Propos/index.jsx
@@ -1,8 +1,33 @@
+import Accordion from '../../components/Accordion'
+import aproposBanner from '../../assets/apropos-banner.jpg'
+import './style.css'
+
 function APropos() {
 	return (
-		<div>
-			<h2>A Propos</h2>
-		</div>
+		<section className="apropos-section">
+			<div className="banner">
+				<img
+					src={aproposBanner}
+					className="banner-img apropos-banner"
+				/>
+			</div>
+			<Accordion
+				title="Fiabilité"
+				content="Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes."
+			/>
+			<Accordion
+				title="Respect"
+				content="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."
+			/>
+			<Accordion
+				title="Service"
+				content="Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question."
+			/>
+			<Accordion
+				title="Sécurité"
+				content="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."
+			/>
+		</section>
 	)
 }
 
