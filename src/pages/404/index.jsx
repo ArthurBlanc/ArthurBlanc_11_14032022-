8fc96919929bdbc5f2c3b84a423e0aa08cbbf859
Arthur BLANC <arthur.blanc@outlook.com>
2022-05-22 15:06:55 +0200

    remove delay for error404

--- src/pages/404/index.jsx
+++ src/pages/404/index.jsx
@@ -5,22 +5,6 @@ import Error from '../../components/Error'
 function Error404() {
 	/* Setting the title of the page. */
 	document.title = '404 - Kasa'
-	/* Creating a state variable called `show` and setting it to `false`. */
-	const [show, setShow] = useState(false)
-
-	useEffect(() => {
-		/* Set a timeout to delay the showing of the loading component. */
-		const timeShow = setTimeout(() => {
-			setShow(true)
-		}, 10)
-		return () => {
-			clearTimeout(timeShow)
-		}
-	}, [])
-
-	if (!show) {
-		return null
-	}
 
 	return (
 		<Error
